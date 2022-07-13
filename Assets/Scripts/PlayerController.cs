1568872d9ae1810975a3364501898aed41092281
VeetheDev <53603573+vuvu25@users.noreply.github.com>
2022-07-14 01:28:54 +0200

    Added camera movement and player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed;
+    public Rigidbody2D theRB;
+    public float jumpForce;
+
+    
+    
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
+        theRB.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), theRB.velocity.y);
+
+      
+        if(Input.GetButtonDown("Jump"))
+        {
         
+            theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+            
+        }
     }
 }
